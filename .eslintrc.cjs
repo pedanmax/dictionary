e018b7b399e7038c332c2daab9ed1f0ed0a2b984
pedanmax <m.pedan2017@gmail.com>
2023-06-20 14:11:31 +0300

    init:instal deps

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'jsx-quotes': 0,
+    'react/function-component-definition': 0,
+    'arrow-body-style': 0,
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.scss';
+
+const App = () => {
+  return (
+    <h1>Hello world</h1>
+  );
+};
+
+export default App;
