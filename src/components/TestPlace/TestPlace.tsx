ad860eedef910d6cdbbd93b4fce1e7801a793272
Pedan <m.pedan2017@gmail.com>
2023-07-12 21:53:22 +0300

    feat:add minimal limit for testing words

--- src/components/TestPlace/TestPlace.tsx
+++ src/components/TestPlace/TestPlace.tsx
@@ -1,14 +1,15 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable max-len */
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Stack, Button, Box } from '@mui/material';
+import { Stack, Button, Box, Typography } from '@mui/material';
 import { actions } from '../../Redux/reducers/stateTest.slice';
 import { actions as testingWords } from '../../Redux/reducers/testingWords.slice';
 import GroupTestItems from './GroupTestItems';
 import { StoreType } from '../../types/types';
 import TestResults from './TestResults';
+import oopsImg from '../../assets/oops.png';
 
-// limit the execution of the test if the words are less than 5
 // add image in test result
 // forbiden typing number
 
@@ -16,11 +17,9 @@ const TestPlace = () => {
   const [swapped, setSwapped] = useState(false);
   const [stateResultButton, setStateResultButton] = useState(false);
   const { isStarted, testFields, resultIsOpen } = useSelector((state: StoreType) => state.stateTest);
-  const words = useSelector((state: StoreType) => state.words);
-  const testGroup = useSelector((state: StoreType) => state.testGroup);
-  const testWords = words?.filter((word) => word.group === testGroup);
+  const testWords = useSelector((state:StoreType) => state.testingWords);
+  const countTestWords = testWords && testWords?.length >= 5;
   const dispatch = useDispatch();
-
   const handleStartTest = () => {
     dispatch(actions.startTest(true));
   };
@@ -55,48 +54,63 @@ const TestPlace = () => {
     } else setStateResultButton(false);
   }, [testFieldsKeys, testWords?.length, testFields]);
 
-  return (
-    <Box sx={{ maxWidth: '1000px', margin: '0 auto' }}>
+  return countTestWords
+    ? (
+      <Box sx={{ maxWidth: '1000px', margin: '0 auto' }}>
+        <Stack
+          direction='row'
+          spacing={2}
+          mb='20px'
+          justifyContent='center'
+        >
+          {buttons.map((button) => {
+            return (
+              <Button
+                key={button.text}
+                sx={{
+                  '&.MuiButton-root': { backgroundColor: '#004668' },
+                  '&.MuiButton-root:hover': { backgroundColor: '#003954' },
+                  '&.Mui-disabled': { backgroundColor: '#516e83' },
+                }}
+                variant='contained'
+                onClick={button.function}
+                disabled={button.disabled}
+              >
+                {button.text}
+              </Button>
+            );
+          })}
+        </Stack>
+        {resultIsOpen && <TestResults />}
+        <Stack
+          spacing={4}
+          sx={{
+            backgroundColor: '#fff',
+            padding: '10px',
+            borderRadius: '5px',
+            boxSizing: 'border-box',
+            maxWidth: '1000px',
+            margin: '0 auto',
+          }}
+        >
+          <GroupTestItems swapped={swapped} />
+        </Stack>
+      </Box>
+    )
+    : (
       <Stack
-        direction='row'
-        spacing={2}
-        mb='20px'
         justifyContent='center'
+        alignItems='center'
       >
-        {buttons.map((button) => {
-          return (
-            <Button
-              key={button.text}
-              sx={{
-                '&.MuiButton-root': { backgroundColor: '#004668' },
-                '&.MuiButton-root:hover': { backgroundColor: '#003954' },
-                '&.Mui-disabled': { backgroundColor: '#516e83' },
-              }}
-              variant='contained'
-              onClick={button.function}
-              disabled={button.disabled}
-            >
-              {button.text}
-            </Button>
-          );
-        })}
+        <Typography component='h3' fontSize='26px' color='primary'>
+          You can't test a group with less than 5 words!
+        </Typography>
+        <Typography component='h3' fontSize='22px' color='primary' mb='20px'>
+          Add more words or change the group!
+        </Typography>
+        <img src={oopsImg} alt='oops' style={{ borderRadius: '10px', maxWidth: '100%' }} />
       </Stack>
-      {resultIsOpen && <TestResults />}
-      <Stack
-        spacing={4}
-        sx={{
-          backgroundColor: '#fff',
-          padding: '10px',
-          borderRadius: '5px',
-          boxSizing: 'border-box',
-          maxWidth: '1000px',
-          margin: '0 auto',
-        }}
-      >
-        <GroupTestItems swapped={swapped} />
-      </Stack>
-    </Box>
-  );
+    );
 };
 
 export default TestPlace;
