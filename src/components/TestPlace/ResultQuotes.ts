a309e2c846bae1d62fd4bf0274188451f2a87289
Pedan <m.pedan2017@gmail.com>
2023-07-25 16:43:57 +0300

    feat:add random quotes depending on result

---
+++ src/components/TestPlace/ResultQuotes.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-useless-escape */
+const quotes = {
+  begginger: ['You need to practice more!', 'The result is disappointing try again!', 'Work on these words again!'],
+  intermediate: ['Not bad, but you can do better!', 'Practice your words and come back again!', 'It\'\s not good, practice more!'],
+  upperIntermediate: ['Good result, but not perfect!', 'Almost perfect!', 'Just a little bit short of the perfect result!'],
+  advanced: ['The maximum result!', 'Wow, maximum result, you\'\re a genius!', 'I never doubted you, you are the best!'],
+};
+
+export default quotes;
--- src/components/TestPlace/TestResults.tsx
+++ src/components/TestPlace/TestResults.tsx
@@ -1,9 +1,13 @@
-/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable no-useless-escape */
+/* eslint-disable max-len */
+/* eslint-disable no-nested-ternary */
+/* eslint-disable no-mixed-operators */
 import { Stack, Typography, IconButton } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import { useSelector, useDispatch } from 'react-redux';
 import { actions as stateTest } from '../../Redux/reducers/stateTest.slice';
 import { StoreType } from '../../types/types';
+import quotes from './ResultQuotes';
 
 const TestResults = ({ handleOutlineInput } : { handleOutlineInput: () => void }) => {
   const dispatch = useDispatch();
@@ -13,6 +17,13 @@ const TestResults = ({ handleOutlineInput } : { handleOutlineInput: () => void }
   const trueTranslationCount = arrayWithRightResult.reduce((acc, next) => acc + next, 0);
 
   const result = `${trueTranslationCount}/${arrayWithRightResult.length}`;
+  const correctPerCent = Math.round(trueTranslationCount / arrayWithRightResult.length * 100);
+  const quotesByResult = correctPerCent < 34 ? quotes.begginger
+    : correctPerCent > 33 && correctPerCent < 67 ? quotes.intermediate
+      : correctPerCent > 66 && correctPerCent < 100 ? quotes.upperIntermediate
+        : correctPerCent === 100 ? quotes.advanced : null;
+  const getRandomQuote = () => quotesByResult && quotesByResult[Math.floor(Math.random() * quotesByResult.length)];
+
   const handleOpen = () => {
     dispatch(stateTest.resultIsOpen(false));
     dispatch(stateTest.resetTest(false));
@@ -45,6 +56,11 @@ const TestResults = ({ handleOutlineInput } : { handleOutlineInput: () => void }
           <CloseIcon />
         </IconButton>
       </Stack>
+      {quotesByResult && (
+        <Typography>
+          {getRandomQuote()}
+        </Typography>
+      )}
     </Stack>
   );
 };
